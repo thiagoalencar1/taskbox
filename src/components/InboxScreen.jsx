b5afa5cb5bbf4e78a356da93b8b26e3f490acd1c
Thiago Alencar <talencar@gmail.com>
2024-01-29 09:25:01 -0300

    finish InboxScreen

--- src/components/InboxScreen.jsx
+++ src/components/InboxScreen.jsx
@@ -11,4 +11,24 @@ export default function InboxScreen() {
     dispatch(fetchTasks());
   }, [])
 
+  if (error) {
+    return (
+      <div className="page lists-show">
+        <div className="wrapper-message">
+          <span className="icon-face-sad" />
+          <p className="title-message">Oh no!</p>
+          <p className="subtitle-message">Something went wrong</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="page lists-show">
+      <nav>
+        <h1 className="title-page">Taskbox</h1>
+      </nav>
+      <TaskList />
+    </div>
+  );
 };
