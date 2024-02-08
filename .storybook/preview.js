65dfda3d817a796af050a524e59ec34fdd55459b
Thiago Alencar <talencar@gmail.com>
2024-02-08 07:51:25 -0300

    build: config mockServiceWorker

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 import '../src/index.css';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+initialize();
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
@@ -11,6 +13,7 @@ const preview = {
       },
     },
   },
+  loaders: [mswLoader],
 };
 
 export default preview;
